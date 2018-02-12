2efa950b2aded5b398e8a005c7d00966cfaa633b
Jonesy <Jonesy@Tryptamine>
2018-02-12 15:29:32 +0000

    Fixed no configuration data on new config set

--- src/Configuration.php
+++ src/Configuration.php
@@ -20,281 +20,304 @@
 
 namespace Trypta\Liquid {
 
+  /**
+   * Basic Configuration Class
+   *
+   * Parses an ini file into a dot seperated array ie. xxx.yyy
+   * xxx is the ini section value, yyy is the key value. further levels can be
+   * added to the key ie. xxx.yyy.zzz or xxx.yyy.zzz.aaa ....
+   *
+   * @method __shutdown Custom Liquid Magic Method called when the application is shutdown and saves configuration
+   *
+   * @package Liquid Framework
+   * @subpackage Core
+   * @category Configuration
+   * @author Jonesy
+   */
+  class Configuration {
+
     /**
-     * Basic Configuration Class
+     * True if the config has change and has not been saved
      *
-     * Parses an ini file into a dot seperated array ie. xxx.yyy
-     * xxx is the ini section value, yyy is the key value. further levels can be
-     * added to the key ie. xxx.yyy.zzz or xxx.yyy.zzz.aaa ....
-     *
-     * @method __shutdown Custom Liquid Magic Method called when the application is shutdown and saves configuration
-     *
-     * @package Liquid Framework
-     * @subpackage Core
-     * @category Configuration
-     * @author Jonesy
+     * @access private
+     * @var boolean $_requiresSave
      */
-    class Configuration
+    private $_requiresSave = false;
+
+    /**
+     * True when the configuration has been read and parsed
+     *
+     * @access private
+     * @var boolean $_loaded
+     */
+    private $_loaded = false;
+
+    /**
+     * Configuration file path
+     *
+     * @access private
+     * @var string $_filename
+     */
+    private $_filename = null;
+
+    /**
+     * Parsed configuration data
+     *
+     * @access private
+     * @var array $_data
+     */
+    private $_data = null;
+
+    /**
+     * Request constructor
+     *
+     * @access public
+     * @param string $filename  - Location of configuration file to load
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($filename = null)
     {
+      if(!$filename || is_null($filename))
+      {
+        throw new \InvalidArgumentException('Configuration filename is required');
+      }
 
-        /**
-         * True if the config has change and has not been saved
-         *
-         * @access private
-         * @var boolean $_requiresSave
-         */
-        private $_requiresSave = false;
-
-        /**
-         * True when the configuration has been read and parsed
-         *
-         * @access private
-         * @var boolean $_loaded
-         */
-        private $_loaded = false;
-
-        /**
-         * Configuration file path
-         *
-         * @access private
-         * @var string $_filename
-         */
-        private $_filename = null;
-
-        /**
-         * Parsed configuration data
-         *
-         * @access private
-         * @var array $_data
-         */
-        private $_data = null;
-
-        /**
-         * Request constructor
-         *
-         * @access public
-         * @param string $filename  - Location of configuration file to load
-         * @throws \InvalidArgumentException
-         */
-        public function __construct($filename = null)
-        {
-            if (!$filename || is_null($filename)) {
-                throw new \InvalidArgumentException('Configuration filename is required');
-            }
-
-            $this->_filename = $filename;
-            if (file_exists($filename)) {
-                $this->load();
-            }
-            
-        }
-
-        /**
-         * Destructor
-         *
-         * Calls the __shutdown method on destruction
-         *
-         * @access public
-         */
-        public function __destruct()
-        {
-            $this->__shutdown();
-        }
-
-        /**
-         * Shutdown method, called when the system is shutdown
-         *
-         * @access public
-         */
-        public function __shutdown()
-        {
-            if ($this->_requiresSave) {
-                $this->save();
-            }
-        }
-
-        /**
-         * Reloads the configuration from disk, saves first if required
-         *
-         * @access public
-         */
-        public function reload()
-        {
-            if ($this->_requiresSave) {
-                $this->save();
-            }
-
-            $this->_loaded = false;
-            $this->load();
-        }
-
-        /**
-         * Loads the configuration if not loaded already
-         *
-         * @access public
-         * @throws RuntimeException
-         */
-        public function load()
-        {
-            if (!$this->_loaded) {
-                $data = parse_ini_file($this->_filename, true, INI_SCANNER_NORMAL);
-
-                foreach ($data as $name => $section) {
-                    $this->_data[$name] = array();
-                    foreach ($section as $key => $value) {
-                        $parts = explode(".", $key);
-
-                        if (count($parts) != 2) {
-                            throw new \RuntimeException('Invalid configuration depth: ' . $name . '.' . implode(".", $parts));
-                        }
-
-                        $a = array_shift($parts);
-                        $b = array_shift($parts);
-
-                        if (!array_key_exists($a, $this->_data[$name])) {
-                            $this->_data[$name][$a] = array();
-                        }
-
-                        $this->_data[$name][$a][$b] = $value;
-                    }
-                }
-
-                $this->_loaded = true;
-            }
-        }
-
-        /**
-         * Saves the configuration to disk
-         *
-         * @access public
-         */
-        public function save()
-        {
-            $data = array();
-            $data[] = "; <?php die; ?>";
-            $data[] = "; ";
-            $data[] = "; Liquid Framework Application Configuration File";
-            $data[] = "";
-
-            foreach ($this->_data as $a => $section) {
-                $data[] = "[" . $name . "]";
-                foreach ($section as $a => $subsection) {
-                    foreach ($subsection as $c => $value) {
-                        $data[] = $b . '.' . $c . '="' . $value . '"';
-                    }
-                }
-
-                file_put_contents($this->_filename, $data);
-                $this->_requiresSave = false;
-            }
-        }
-
-        /**
-         * Gets a configuration value
-         *
-         * @access public
-         * @param string $key - dot seperated config key
-         * @return mixed - The value
-         * @throws \InvalidArgumentException
-         */
-        public function get($key)
-        {
-            $parts = explode(".", $key);
-            $n = count($parts);
-
-            if ($n < 1 || $n > 3) {
-                throw new \InvalidArgumentException('Configuration key must be between 1 and 3 levels in depth: ' . $key);
-            }
-
-            $a = array_shift($parts);
-            $b = count($parts) > 0 ? array_shift($parts) : false;
-            $c = count($parts) > 0 ? array_shift($parts) : false;
-
-            if (!array_key_exists($a, $this->_data)) {
-                throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
-            }
-
-            if (!$b) {
-                return $this->_data[$a];
-            }
-
-            if (!array_key_exists($b, $this->_data[$a])) {
-                throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
-            }
-
-            if (!$c) {
-                return $this->_data[$a][$b];
-            }
-
-            if (!array_key_exists($c, $this->_data[$a][$b])) {
-                throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
-            }
-
-            return $this->_data[$a][$b][$c];
-        }
-
-        /**
-         * Sets a configuration value
-         *
-         * @access public
-         * @param string $key - dot seperated configuration key
-         * @param mixed $value - The value to set
-         * @throws \InvalidArgumentException
-         */
-        public function set($key, $value)
-        {
-            $parts = explode(".", $key);
-            $n = count($parts);
-
-            if ($n < 1 || $n > 3) {
-                throw new \InvalidArgumentException('Configuration key must be between 1 and 3 levels in depth: ' . $key);
-            }
-
-            $a = array_shift($parts);
-            $b = count($parts) > 0 ? array_shift($parts) : false;
-            $c = count($parts) > 0 ? array_shift($parts) : false;
-
-            if(!is_array($this->_data))
-            {
-              $this->_data = array();
-            }
-            
-            if (!array_key_exists($a, $this->_data)) {
-                throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
-            }
-
-            if (!$b) {
-                if (!is_array($value)) {
-                    throw new \InvalidArgumentException('1st level configiration data must be an array');
-                }
-                foreach ($value as $k => $v) {
-                    $this->set($a . '.' . $k, $v);
-                }
-            }
-
-            if (!array_key_exists($b, $this->_data[$a])) {
-                throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
-            }
-
-            if (!$c) {
-                if (!is_array($value)) {
-                    throw new \InvalidArgumentException('2nd level configiration data must be an array');
-                }
-                foreach ($value as $k => $v) {
-                    $this->set($a . '.' . $b . '.' . $k, $v);
-                }
-            }
-
-            if (!array_key_exists($c, $this->_data[$a][$b])) {
-                throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
-            }
-
-            if (is_array($value)) {
-                throw new \InvalidArgumentException('3rd level configiration data cannot be an array');
-            }
-            $this->requiresSave = $this->_data[$a][$b][$c] == $value ? false : true;
-            $this->_data[$a][$b][$c] = $value;
-        }
+      $this->_filename = $filename;
+      if(file_exists($filename))
+      {
+        $this->load();
+      }
     }
+
+    /**
+     * Destructor
+     *
+     * Calls the __shutdown method on destruction
+     *
+     * @access public
+     */
+    public function __destruct()
+    {
+      $this->__shutdown();
+    }
+
+    /**
+     * Shutdown method, called when the system is shutdown
+     *
+     * @access public
+     */
+    public function __shutdown()
+    {
+      if($this->_requiresSave)
+      {
+        $this->save();
+      }
+    }
+
+    /**
+     * Reloads the configuration from disk, saves first if required
+     *
+     * @access public
+     */
+    public function reload()
+    {
+      if($this->_requiresSave)
+      {
+        $this->save();
+      }
+
+      $this->_loaded = false;
+      $this->load();
+    }
+
+    /**
+     * Loads the configuration if not loaded already
+     *
+     * @access public
+     * @throws RuntimeException
+     */
+    public function load()
+    {
+      if(!$this->_loaded)
+      {
+        $data = parse_ini_file($this->_filename, true, INI_SCANNER_NORMAL);
+
+        foreach($data as $name => $section)
+        {
+          $this->_data[$name] = array();
+          foreach($section as $key => $value)
+          {
+            $parts = explode(".", $key);
+
+            if(count($parts) != 2)
+            {
+              throw new \RuntimeException('Invalid configuration depth: ' . $name . '.' . implode(".", $parts));
+            }
+
+            $a = array_shift($parts);
+            $b = array_shift($parts);
+
+            if(!array_key_exists($a, $this->_data[$name]))
+            {
+              $this->_data[$name][$a] = array();
+            }
+
+            $this->_data[$name][$a][$b] = $value;
+          }
+        }
+
+        $this->_loaded = true;
+      }
+    }
+
+    /**
+     * Saves the configuration to disk
+     *
+     * @access public
+     */
+    public function save()
+    {
+      $data = array();
+      $data[] = "; <?php die; ?>";
+      $data[] = "; ";
+      $data[] = "; Liquid Framework Application Configuration File";
+      $data[] = "";
+
+      foreach($this->_data as $a => $section)
+      {
+        $data[] = "[" . $name . "]";
+        foreach($section as $a => $subsection)
+        {
+          foreach($subsection as $c => $value)
+          {
+            $data[] = $b . '.' . $c . '="' . $value . '"';
+          }
+        }
+
+        file_put_contents($this->_filename, $data);
+        $this->_requiresSave = false;
+      }
+    }
+
+    /**
+     * Gets a configuration value
+     *
+     * @access public
+     * @param string $key - dot seperated config key
+     * @return mixed - The value
+     * @throws \InvalidArgumentException
+     */
+    public function get($key)
+    {
+      $parts = explode(".", $key);
+      $n = count($parts);
+
+      if($n < 1 || $n > 3)
+      {
+        throw new \InvalidArgumentException('Configuration key must be between 1 and 3 levels in depth: ' . $key);
+      }
+
+      $a = array_shift($parts);
+      $b = count($parts) > 0 ? array_shift($parts) : false;
+      $c = count($parts) > 0 ? array_shift($parts) : false;
+
+      if(!array_key_exists($a, $this->_data))
+      {
+        throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
+      }
+
+      if(!$b)
+      {
+        return $this->_data[$a];
+      }
+
+      if(!array_key_exists($b, $this->_data[$a]))
+      {
+        throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
+      }
+
+      if(!$c)
+      {
+        return $this->_data[$a][$b];
+      }
+
+      if(!array_key_exists($c, $this->_data[$a][$b]))
+      {
+        throw new \InvalidArgumentException('Configuration key does not exist: ' . $key);
+      }
+
+      return $this->_data[$a][$b][$c];
+    }
+
+    /**
+     * Sets a configuration value
+     *
+     * @access public
+     * @param string $key - dot seperated configuration key
+     * @param mixed $value - The value to set
+     * @throws \InvalidArgumentException
+     */
+    public function set($key, $value)
+    {
+      $parts = explode(".", $key);
+      $n = count($parts);
+
+      if($n < 1 || $n > 3)
+      {
+        throw new \InvalidArgumentException('Configuration key must be between 1 and 3 levels in depth: ' . $key);
+      }
+
+      $a = array_shift($parts);
+      $b = count($parts) > 0 ? array_shift($parts) : false;
+      $c = count($parts) > 0 ? array_shift($parts) : false;
+
+      if(!is_array($this->_data))
+      {
+        $this->_data = array();
+      }
+
+      if(!array_key_exists($a, $this->_data))
+      {
+        $this->_data[$a] = array();
+      }
+
+      if(!$b)
+      {
+        if(!is_array($value))
+        {
+          throw new \InvalidArgumentException('1st level configiration data must be an array');
+        }
+        foreach($value as $k => $v)
+        {
+          return $this->set($a . '.' . $k, $v);
+        }
+      }
+
+      if(!array_key_exists($b, $this->_data[$a]))
+      {
+        $this->_data[$a][$b] = array();
+      }
+
+      if(!$c)
+      {
+        if(!is_array($value))
+        {
+          throw new \InvalidArgumentException('2nd level configiration data must be an array');
+        }
+        foreach($value as $k => $v)
+        {
+          return $this->set($a . '.' . $b . '.' . $k, $v);
+        }
+      }
+
+      if(is_array($value))
+      {
+        throw new \InvalidArgumentException('3rd level configiration data cannot be an array');
+      }
+      $this->requiresSave = $this->_data[$a][$b][$c] == $value ? false : true;
+      $this->_data[$a][$b][$c] = $value;
+    }
+
+  }
 
 }
